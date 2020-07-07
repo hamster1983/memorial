fe556acfc698437faf81e98f92ddd5dee4bde02c
Наймушин Егор <webmaster@farexpo.local>
2020-07-07 18:28:00 +0300

    version 07.07

--- js/scripts.js
+++ js/scripts.js
@@ -2,62 +2,34 @@ $(document).ready(function(){
 	
 	$('[name="phone"]').inputmask("+7(999) 999-9999");
 
-	$('.top-slider').slick({
-	  infinite: true,
-	  autoplay: true,
-	  fade: true,
-	  autoplaySpeed: 1500,
-	  arrows: false,
-	  dots: false,
-	  speed: 1500,
-	  slidesToShow: 1,
-	  slidesToScroll: 1
-	});
-
-	$('.video-wrap').click(function() {
-		if($(this).hasClass('prostota-video')) {
-			$(this).html('<iframe src="https://www.youtube.com/embed/2uU6LigXKMI?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-			$('.video-wrap').not(this).html('<img src="img/play.png" alt="" class="play">');
-		}
-		else if($(this).hasClass('remont-video')) {
-			$(this).html('<iframe src="https://www.youtube.com/embed/ZT-0CF9IXko?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-			$('.video-wrap').not(this).html('<img src="img/play.png" alt="" class="play">');
-		}
-	});
-
-	$('.works-slider').slick({
+	$('.primery-slider').slick({
 	  infinite: true,
 	  autoplay: true,
 	  arrows: true,
 	  dots: false,
 	  speed: 1500,
-	  slidesToShow: 3,
+	  slidesToShow: 4,
 	  slidesToScroll: 1,
 	  responsive: [
 		{
 		  breakpoint: 1199,
 		  settings: {
-			slidesToShow: 2,
+			slidesToShow: 3,
 		  }
 		},
 		{
 		  breakpoint: 991,
 		  settings: {
+			slidesToShow: 2,
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
 			slidesToShow: 1,
 		  }
 		}
 	  ] 
-	});
-
-	$('.movie-wrap').click(function() {
-		if($(this).hasClass('left-movie')) {
-			$(this).html('<iframe src="https://www.youtube.com/embed/33Xo5KheqJ4?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-			$('.movie-wrap').not(this).html('<img src="img/big-play.png" alt="" class="big-play">');
-		}
-		else if($(this).hasClass('right-movie')) {
-			$(this).html('<iframe src="https://www.youtube.com/embed/Ro5sADYiHH8?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-			$('.movie-wrap').not(this).html('<img src="img/big-play.png" alt="" class="big-play">');
-		}
 	});
 
 	$('[type="submit"]').click(function(e) {
