34e68f926f33f0123f4563adef89bbcaf0a9efb5
Наймушин Егор <webmaster@farexpo.local>
2020-07-08 17:10:08 +0300

    version 08.07 - 2

--- js/scripts.js
+++ js/scripts.js
@@ -48,61 +48,18 @@ $(document).ready(function(){
 		  success: function(data) {
 			$('#result .modal-body').html(data);
 			$('#result').modal('show');
-			//$('form [type="text"]').val('');
 		  }
 		});
 	});
-
-	/*$('.faq-question').click(function(){
-	    $(this).find('.faq-thumb').toggleClass('active');
-	});
-
-	$('.comments-slider').slick({
-	  infinite: true,
-	  autoplay: false,
-	  autoplaySpeed: 1500,
-	  arrows: false,
-	  dots: false,
-	  speed: 1500,
-	  slidesToShow: 1,
-	  slidesToScroll: 1
-	});
-
-	$('.comments-prev').click(function() {
-		$('.comments-slider').slick('slickPrev');
-	});
-	$('.comments-next').click(function() {
-		$('.comments-slider').slick('slickNext');
-	});
-	$('.comments-slider').on('afterChange', function(event, slick, currentSlide, nextSlide) {
-		$('.comments-num-slide .current').text(currentSlide+1);
-	});
-
-	$('[type="submit"]').click(function(e) {
-		let form = $(this).parents('form');
-		if(form.find('[name="phone"]').val() == '' || form.find('[name="name"]').val() == '') {
-			alert('Поля ИМЯ и ТЕЛЕФОН не могут быть пустыми! Заявка НЕ отправлена! Попробуйте еще раз');
-			form.find('[type="text"]')[0].focus();
-			return false;
-		}
-		e.preventDefault();
-		$.ajax({
-		  cashe: false,
-		  type: form.attr('method'),
-		  url: form.attr('action'),
-		  data: form.serialize(),
-		  success: function(data) {
-			$('#result .modal-body').html(data);
-			$('#result').modal('show');
-			//$('form [type="text"]').val('');
-		  }
-		});
-	});*/
 	
-	/*$('#plitka-quiz').on('slid.bs.carousel', function(){
+	$('#quiz-btn').click(function(){
+		$('.calculate').slideDown('fast');
+	});
+	
+	$('#memory-quiz').on('slid.bs.carousel', function(){
 	    $('#current').text(+$('.carousel-indicators li.active').attr('data-slide-to')+1);
 	});
-	$('#plitka-quiz .btn-yellow').not('[type="submit"], .back').click(function(){
+	$('#memory-quiz .btn-braun').not('[type="submit"]').click(function(){
 		let parent = $(this).closest('.question');
 		let title = parent.attr('data-title');
 		let result = '';
@@ -118,65 +75,15 @@ $(document).ready(function(){
 			}
 		});
 		if(result == '') {
-			alert('Ответьте на вопрос');
+			alert('Пожалуйста, ответьте на вопрос');
 			parent.find('input').focus();
 			return false;
 		}
-		$('#plitka-quiz [type="hidden"]').each(function(){
+		$('#memory-quiz [type="hidden"]').each(function(){
 			if($(this).attr('name') == title) {
 				$(this).val(result);
 			}
 		});
 	});
-	
-	$('.top-slider').slick({
-	  infinite: true,
-	  autoplay: true,
-	  fade: true,
-	  autoplaySpeed: 2000,
-	  arrows: false,
-	  dots: false,
-	  speed: 2000,
-	  slidesToShow: 1,
-	  slidesToScroll: 1
-	});
-	
-	$('.part-slider').slick({
-	  infinite: true,
-	  autoplay: true,
-	  arrows: true,
-	  dots: false,
-	  speed: 1500,
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  responsive: [
-		{
-		  breakpoint: 1199,
-		  settings: {
-			slidesToShow: 2,
-		  }
-		},
-		{
-		  breakpoint: 991,
-		  settings: {
-			slidesToShow: 1,
-		  }
-		}
-	  ] 
-	});
-	
-	 $('.comments-slider-for').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  arrows: false,
-	  fade: true,
-	  asNavFor: '.comments-slider-nav'
-	});
-	$('.comments-slider-nav').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  asNavFor: '.comments-slider-for',
-	  arrows: false
-	});*/
 	
 });
